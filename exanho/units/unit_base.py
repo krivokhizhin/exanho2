404e41a8cb9362a32b6236d9d99717234e1e5040
Konstantin K <smartgip@mail.ru>
2020-03-10 23:03:16 +0300

    excluded double logging

--- exanho/units/unit_base.py
+++ exanho/units/unit_base.py
@@ -16,7 +16,7 @@ class ExanhoUnitBase(ABC):
 
     def _configurer_logging(self, queue):
         h = logging.handlers.QueueHandler(queue)  # Just the one handler needed
-        root = logging.getLogger()
+        root = logging.getLogger('root')
         root.addHandler(h)
         root.setLevel(logging.DEBUG)
 

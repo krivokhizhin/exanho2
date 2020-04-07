fdeddb32a5bd4bfd522adcfe72e994142e0641a9
Konstantin K <smartgip@mail.ru>
2020-04-07 20:13:26 +0300

    code review

--- exanho/sandbox/import_module.py
+++ exanho/sandbox/import_module.py
@@ -1,29 +1,29 @@
 import importlib
 
-from abc import ABCMeta
-from exanho.core.actors import ServiceBase
+# from abc import ABCMeta
+# from exanho.core.actors import ServiceBase
 
 
-module_path = 'exanho.services.nsi.organization_type' # 'exanho.orm.sqlalchemy'
+module_path = 'exanho.workers.loading' # 'exanho.orm.sqlalchemy'
 url = 'postgresql+psycopg2://kks:Nata1311@localhost/eis44'
 
 
 def run():
     mod = importlib.import_module(module_path)
 
-    hosting_service = None   
-    for service_name, service_class in vars(mod).items():
-        if ((type(service_class) == type or type(service_class) == ABCMeta)
-        and issubclass(service_class, ServiceBase)
-        and service_name != ServiceBase.__name__):
-            hosting_service = service_class
+    # hosting_service = None   
+    # for service_name, service_class in vars(mod).items():
+    #     if ((type(service_class) == type or type(service_class) == ABCMeta)
+    #     and issubclass(service_class, ServiceBase)
+    #     and service_name != ServiceBase.__name__):
+    #         hosting_service = service_class
 
-    print(hosting_service)
+    # print(hosting_service)
 
-    service = hosting_service()
-    print(service.put('Dd', 'ddddd'))
+    # service = hosting_service()
+    # print(service.put('Dd', 'ddddd'))
 
     mod.domain.configure(url)
-    # print(mod.validate(url))
+    # print(mod.domain.validate(url))
     mod.domain.recreate(url)
     print('complete')

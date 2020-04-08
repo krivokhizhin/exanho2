e9a7a8a8015c4979a7d36ffbedefede1bbc7fa28
Konstantin K <smartgip@mail.ru>
2020-04-08 21:29:25 +0300

    refactoring loading models

--- exanho/model/loading.py
+++ exanho/model/loading.py
@@ -1,51 +1,37 @@
+from enum import Enum
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from ..orm.sqlalchemy import Base
 from .i_serialize import ISerializeToDict
 
-class LoadStatus(Base, ISerializeToDict):
-    __tablename__ = 'load_status'
-    
-    id = Column(Integer, primary_key=True)
-    code = Column(String(20), index=True, nullable=False, unique=True)
-    name = Column(String(100))
-    description = Column(String(500))
+class LoadStatus(Enum):
+    NEW = 'NEW'
+    DIRTY = 'DIRTY'
+    COMPLETE = 'COMPLETE'
 
-    def __init__(self, code, name, desc=None):
-        self.code = code
-        self.name = name
-        self.description = desc
-
-    def __str__(self):
-        return 'LoadStatus, {0.id}: code={0.code}, name={0.name}'.format(self)
-
-    def __repr__(self):
-        return 'LoadStatus({0.code}, {0.name}, {0.description})'.format(self)
-
-    def serialize(self):
-        dto = super().serialize()
-        return dto.update({'id': self.id, 'code':self.code, 'name':self.name, 'description':self.description})
 
 class LoadTask(Base, ISerializeToDict):
     __tablename__ = 'load_task'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
-    status_id = Column(Integer, ForeignKey(LoadStatus.id), nullable=False) # ForeignKey('load_task.id')
+    status = Column(String(20), nullable=False)
     location = Column(String(500), nullable=False)
-    scheduled_date = Column(DateTime)
+    scheduled_date = Column(DateTime, nullable=False)
     description = Column(String(500))
 
     def __init__(self, name, location, desc=None):
         self.code = code
         self.name = name
+        self.status = LoadStatus.NEW.value
         self.description = desc
 
     def __str__(self):
-        return 'LoadTask, {0.id}: name={0.name}, status_id={0.status_id}, location={0.location}, scheduled_date={0.scheduled_date}'.format(self)
+        return 'LoadTask, {0.id}: name={0.name}, status={0.status}, location={0.location}, scheduled_date={0.scheduled_date}'.format(self)
 
     def __repr__(self):
-        return 'LoadTask(name={0.name}, status_id={0.status_id}, location={0.location}, scheduled_date={0.scheduled_date}, description={0.description})'.format(self)
+        return 'LoadTask(name={0.name}, status={0.status}, location={0.location}, scheduled_date={0.scheduled_date}, description={0.description})'.format(self)
 
     def serialize(self):
         dto = super().serialize()
@@ -53,7 +39,7 @@ class LoadTask(Base, ISerializeToDict):
             'id':self.id,
             'name':self.name,
             'location':self.location,
-            'status_id':self.status_id,
+            'status':self.status,
             'scheduled_date':self.scheduled_date,
             'description':self.description
             })
@@ -63,23 +49,25 @@ class LoadArchive(Base, ISerializeToDict):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
-    status_id = Column(Integer, ForeignKey(LoadStatus.id), nullable=False) # ForeignKey('load_task.id')
+    status = Column(String(20), nullable=False)
     location = Column(String(500), nullable=False)
     checksum = Column(String(50))
     update_date = Column(DateTime)
 
+    files = relationship('LoadFile', back_populates='archive')
+
     def __str__(self):
-        return 'LoadArchive, {0.id}: name={0.name}, status_id={0.status_id}, location={0.location}'.format(self)
+        return 'LoadArchive, {0.id}: name={0.name}, status={0.status}, location={0.location}'.format(self)
 
     def __repr__(self):
-        return 'LoadArchive(name={0.name}, status_id={0.status_id}, location={0.location})'.format(self)
+        return 'LoadArchive(name={0.name}, status={0.status}, location={0.location})'.format(self)
 
     def serialize(self):
         dto = super().serialize()
         return dto.update({
             'id':self.id,
             'name':self.name,
-            'status_id':self.status_id,
+            'status':self.status,
             'location':self.location,
             'checksum':self.checksum,
             'update_date':self.update_date
@@ -89,17 +77,19 @@ class LoadFile(Base, ISerializeToDict):
     __tablename__ = 'load_file'
 
     id = Column(Integer, primary_key=True)
-    archive_id = Column(Integer, ForeignKey(LoadArchive.id)) # ForeignKey('load_archive.id')
+    archive_id = Column(Integer, ForeignKey(LoadArchive.id))
     name = Column(String(256), nullable=False)
-    status_id = Column(Integer, ForeignKey(LoadStatus.id), nullable=False) # ForeignKey('load_task.id')
+    status = Column(String(20), nullable=False)
     checksum = Column(String(50))
     update_date = Column(DateTime)
 
+    archive = relationship('LoadArchive', back_populates='files')
+
     def __str__(self):
-        return 'LoadFile, {0.id}: archive_id={0.archive_id}, name={0.name}, status_id={0.status_id}'.format(self)
+        return 'LoadFile, {0.id}: archive_id={0.archive_id}, name={0.name}, status={0.status}'.format(self)
 
     def __repr__(self):
-        return 'LoadFile(archive_id={0.archive_id}, name={0.name}, status_id={0.status_id}'.format(self)
+        return 'LoadFile(archive_id={0.archive_id}, name={0.name}, status={0.status}'.format(self)
 
     def serialize(self):
         dto = super().serialize()
@@ -107,7 +97,7 @@ class LoadFile(Base, ISerializeToDict):
             'id':self.id,
             'archive_id':self.archive_id,
             'name':self.name,
-            'status_id':self.status_id,
+            'status':self.status,
             'checksum':self.checksum,
             'update_date':self.update_date
             })
--- exanho/workers/loading.py
+++ exanho/workers/loading.py
@@ -1,10 +1,16 @@
 import logging
+import datetime
 
 import exanho.orm.sqlalchemy as domain
 
 from exanho.model.loading import LoadStatus, LoadTask, LoadArchive, LoadFile
 
 log = logging.getLogger(__name__)
+
+class Context:
+    pass
+
+context = Context()
 
 def initialize(*args, **kwargs):
     db_url = kwargs.get('db_url', None)
@@ -20,9 +26,16 @@ def initialize(*args, **kwargs):
             
         domain.configure(db_url)
 
+        context.session = domain.Session()
+
     log.info(f'initialize')
 
-def work():
+def work():    
+    now = datetime.datetime.now()
+    for load_task in context.session.query(LoadTask).\
+        filter(LoadTask.scheduled_date < now).\
+        filter(LoadTask.status == LoadStatus.NEW.value):
+        log.debug(load_task)
     log.info(f'work')
 
 def finalize():

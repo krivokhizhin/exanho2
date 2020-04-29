e272d6ae3438e16b8b88ded1c5cf5b3938f4c30a
Konstantin K <smartgip@mail.ru>
2020-04-29 12:33:42 +0300

    added methods to dt_utilities

--- exanho/core/common/dt_utilities.py
+++ exanho/core/common/dt_utilities.py
@@ -5,3 +5,15 @@ def check_none_return_min(dt:datetime=None):
 
 def check_none_return_max(dt:datetime=None):
     return dt if dt else datetime.datetime(datetime.MAXYEAR, 12, 31)
+
+def max_or_none(val1, val2):
+    if val1 is None and val2 is None:
+        return None
+
+    return max([check_none_return_min(val1), check_none_return_min(val2)])
+
+def min_or_none(val1, val2):
+    if val1 is None and val2 is None:
+        return None
+
+    return min([check_none_return_max(val1), check_none_return_max(val2)])

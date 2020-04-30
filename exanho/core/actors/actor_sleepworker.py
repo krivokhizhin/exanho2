7fd6fdb0b4bd6b9daa65c244c3141b5a2efb8d05
Konstantin K <smartgip@mail.ru>
2020-04-30 18:59:03 +0300

    code review

--- exanho/core/actors/actor_sleepworker.py
+++ exanho/core/actors/actor_sleepworker.py
@@ -21,20 +21,20 @@ class SleepWorker(Actor):
             if worker_config.concurrency:
                 if worker_config.concurrency.kind.lower() == 'process':
                     for n in range(worker_config.concurrency.degree):
-                        p = multiprocessing.Process(target=self.start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_process_terminated, self._log_queue), name=f'Process#{n}')
+                        p = multiprocessing.Process(target=start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_process_terminated, self._log_queue), name=f'{self.config.name}_Pr#{n}')
                         p.daemon = worker_config.concurrency.daemon
                         p.start()
                         self.workers.append(p)
                 elif worker_config.concurrency.kind.lower() == 'thread':
                     for n in range(worker_config.concurrency.degree):
-                        t = threading.Thread(target=self.start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_thread_terminated, None), name=f'Thread#{n}')
+                        t = threading.Thread(target=start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_thread_terminated, None), name=f'{self.config.name}_Thr#{n}')
                         t.daemon = worker_config.concurrency.daemon
                         t.start()
                         self.workers.append(t)
                 else:
                     raise Exception(f'The concurrency_type is "{worker_config.concurrency.kind}". There must be either "Thread" or "Process"')
             else:
-                t = threading.Thread(target=self.start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_thread_terminated, None))
+                t = threading.Thread(target=start_worker, args=(worker_config.module, worker_config.sleep, worker_config.appsettings, self.worker_thread_terminated, None), name=f'{self.config.name}_Thr_single')
                 t.daemon = True
                 t.start()
                 self.workers.append(t)
@@ -51,24 +51,24 @@ class SleepWorker(Actor):
     def handle(self):
         pass
 
-    def start_worker(self, worker_module, timeout, appsettings, event, log_queue):
-        if log_queue:
-            from exanho.core.common.log_utilities import configurer_logging
-            configurer_logging(log_queue)
+def start_worker(worker_module, timeout, appsettings, event, log_queue):
+    if log_queue:
+        from exanho.core.common.log_utilities import configurer_logging
+        configurer_logging(log_queue)
 
-        log = logging.getLogger(worker_module)  
+    log = logging.getLogger(worker_module)
 
-        import importlib
-        mod = importlib.import_module(worker_module)
+    import importlib
+    mod = importlib.import_module(worker_module)
 
-        context = mod.initialize(appsettings.__dict__ if appsettings else None)
+    context = mod.initialize(appsettings.__dict__ if appsettings else None)
 
-        while not event.wait(timeout):
-            try:
-                context = mod.work(context)
-            except Exception as ex:
-                log.exception(ex)
+    while not event.wait(timeout):
+        try:
+            context = mod.work(context)
+        except Exception as ex:
+            log.exception(ex)
 
-        mod.finalize(context)
+    mod.finalize(context)
 
 

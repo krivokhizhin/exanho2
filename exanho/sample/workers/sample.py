08db079613cc56f35a491a0905887de70078aa82
Konstantin K <smartgip@mail.ru>
2020-05-25 12:29:56 +0300

    code review

--- exanho/sample/workers/sample.py
+++ exanho/sample/workers/sample.py
@@ -3,28 +3,24 @@ import random
 import string
 
 from collections import defaultdict, namedtuple
-from multiprocessing import JoinableQueue
+# from multiprocessing import JoinableQueue
 
 log = logging.getLogger(__name__)
-
-queues = defaultdict(JoinableQueue)
 
 Context = namedtuple('Context', ['joinable_queue'])
 
 def initialize(appsettings=None, **joinable_queues):
     log.info(f'initialize')
-    random.seed()
-    if joinable_queues:
-        queues.update(**joinable_queues)
-
     context = Context(**appsettings)
+    random.seed()
+    context = context._replace(joinable_queue=joinable_queues.get(context.joinable_queue))
     return context
 
 def work(context:Context):
     log.info(f'work')
 
     task_number = ''.join([random.choice(string.hexdigits) for _ in range(10)])
-    queues.get(context.joinable_queue).put(task_number)
+    context.joinable_queue.put(task_number)
     log.info(f'Task "{task_number}" has been assigned.')
 
     return context

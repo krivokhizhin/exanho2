2b3122c4765b830a29c419c0f94247d248607d9a
Konstantin K <smartgip@mail.ru>
2021-01-27 20:39:13 +0300

    code review

--- exanho/eis44/sql/DROP_contract.sql
+++ exanho/eis44/sql/DROP_contract.sql
@@ -1,5 +1,6 @@
 DROP TABLE zfcs_contract_procedure_cancel2015 CASCADE;
 DROP TABLE zfcs_contract_procedure2015 CASCADE;
+DROP TABLE zfcs_contract_cancel2015 CASCADE;
 
 DROP TABLE zfcs_contract2015_supplier CASCADE;
 DROP TABLE cntr_contact CASCADE;

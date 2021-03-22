3fef7ccdc2b7edff5b7696d5497261e7f5b7ac88
Konstantin K <smartgip@mail.ru>
2021-03-22 18:33:54 +0300

    code review

--- exanho/eis44/sql/INSERT.sql
+++ exanho/eis44/sql/INSERT.sql
@@ -3,4 +3,6 @@ VALUES('NEW', '/fcs_regions/Adygeja_Resp/contracts/*', '*', date '2021-01-01' + 
 INSERT INTO ftp_load_task (status, location, schedule, scheduled_date, min_date, excluded_folders)
 VALUES('NEW', '/fcs_regions/Altaj_Resp/contracts/*', '*', date '2021-01-01' + time '08:00:00', date '2017-12-31' + time '08:00:00', 'prevMonth');
 INSERT INTO ftp_load_task (status, location, schedule, scheduled_date, min_date, excluded_folders)
+VALUES('NEW', '/fcs_regions/Altajskij_kraj/contracts/*', '*', date '2021-01-01' + time '08:00:00', date '2017-12-31' + time '08:00:00', 'prevMonth');
+INSERT INTO ftp_load_task (status, location, schedule, scheduled_date, min_date, excluded_folders)
 VALUES('NEW', '/fcs_regions/Irkutskaja_obl/contracts/*', '*', date '2021-01-01' + time '08:00:00', date '2019-12-31' + time '08:00:00', 'prevMonth');

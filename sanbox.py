daaee3a48761afdd968111f5e22ae6d19a910e36
Konstantin K <smartgip@mail.ru>
2021-04-13 10:14:42 +0300

    code review

--- sanbox.py
+++ sanbox.py
@@ -1,4 +1,5 @@
-from exanho.purchbot.seed import seed as mod
+# from exanho.purchbot.seed import seed as mod
+from exanho.sandbox import rpc_client as mod
 
 if __name__ == '__main__':
     mod.run()
